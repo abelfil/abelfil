ab52d8b7798d92f1691b0b8cefdd144a9b16272b
abelfil <abelfb@students.nu-lipa.edu.ph>
2025-04-15 11:36:50 +0800

    add

---
+++ Activity 1/TestVehicle.java
@@ -0,0 +1,13 @@
+public class Vehicle{
+
+}
+public static void main(String[] args){
+
+    System.out.println("Pogi ako");
+    
+
+    System.out.println("brand");
+    System.out.println("speed");
+    System.out.println("fuelType");
+
+}
